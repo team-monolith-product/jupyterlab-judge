d28fbe8b60784e7b16239373b81f674011917007
Paul Kim <paulkim3151@gmail.com>
2024-06-05 15:58:23 +0900

    refactor: Parse JSON response data in requestAPI function

--- src/handler.ts
+++ src/handler.ts
@@ -29,17 +29,11 @@ export async function requestAPI<T>(
   }
 
   let data: any = await response.text();
-
-  if (data.length > 0) {
-    try {
-      data = JSON.parse(data);
-    } catch (error) {
-      console.log('Not a JSON response body.', response);
-    }
-  }
-
   if (!response.ok) {
     throw new ServerConnection.ResponseError(response, data.message || data);
+  }
+  if (data.length > 0) {
+    data = JSON.parse(data);
   }
 
   return data;
