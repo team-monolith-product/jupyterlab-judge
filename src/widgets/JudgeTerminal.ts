728090100e772f61cb72f6e1df9425e9b92ce1e6
Paul Kim <paulkim3151@gmail.com>
2025-10-02 11:27:45 +0900

    feat: import SVG icon and add type declaration for SVG files

--- src/widgets/JudgeTerminal.ts
+++ src/widgets/JudgeTerminal.ts
@@ -3,7 +3,7 @@ import { JudgeOutputArea } from './JudgeOutputArea';
 import { LabIcon, runIcon, stopIcon } from '@jupyterlab/ui-components';
 import { TRANSLATOR_DOMAIN } from '../constants';
 import { JudgePanel } from './JudgePanel';
-import { cornerUpLeftDoubleFillSvg } from '@team-monolith/cds';
+import cornerUpLeftDoubleFillSvg from '../icons/corner-up-left-double-fill.svg';
 
 const resetIcon = new LabIcon({
   name: 'judge-icon:reset',
